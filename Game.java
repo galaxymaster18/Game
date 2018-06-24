761eb358d935da1228211c48ac51feb0efdf3b08
galaxymaster18 <36861550+galaxymaster18@users.noreply.github.com>
2018-06-24 12:33:12 +0300

    Add files via upload

--- Game.java
+++ Game.java
@@ -11,48 +11,49 @@ public class Game {
 
   public static void main(String[] args) {
     while (true) {
-
       a = new byte[5];
       ++a[(byte) r.nextInt(3)];
       a[4] = ans();
       System.out.print("Do you wanna know wrong one?\n");
-      if (YN()) System.out.print((wrng() + 1) + " is wrong.\nDo you wanna change your choice?\n");
       if (YN()) {
-        System.out.print("What's your new choice?\n");
-        a[4] = ans();
+        System.out.print((wrng() + 1) + " is wrong.\nDo you wanna change your choice?\n");
+        if (YN()) {
+          System.out.print("What's your new choice?\n");
+          a[4] = ans();
+        }
       }
       System.out.print("You " + ((a[4] == 1) ? "win!" : "lose.") + "\nDo you wanna replay?\n");
-      if (!YN()) {
-        System.out.print("Thanks for playing!");
-        break;
-      }
+      if (!YN()) System.exit(0);
     }
   }
 
   public static boolean YN() {
-    while (true) {
-      System.out.print("yes/no\n");
-      switch (sc.next()) {
-        case "yes":
-          return true;
-        case "no":
-          return false;
-      }
+    System.out.print("yes/no\n");
+    switch (sc.next()) {
+      case "yes":
+        return true;
+      case "no":
+        return false;
+      default:
+        return YN();
     }
   }
 
   public static byte ans() {
-    while (true) {
-      System.out.print("Input 1, 2 or 3\n");
-      a[3] = sc.nextByte();
-      return a[a[3] - 1];
+    System.out.print("Input 1, 2 or 3\n");
+    switch (a[3] = sc.nextByte()) {
+      case 1:
+      case 2:
+      case 3:
+        return a[a[3] - 1];
+      default:
+        return ans();
     }
   }
 
   public static byte wrng() {
-    while (true) {
-      byte k = (byte) r.nextInt(3);
-      if (a[k] == 0 && a[3] != k + 1) return k;
-    }
+    byte k = (byte) r.nextInt(3);
+    if (a[k] == 0 && a[3] != k + 1) return k;
+    else return wrng();
   }
 }
